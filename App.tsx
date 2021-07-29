5db8d93c929cfd9507fd3d48f7d2ae80846f0609
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-07-29 19:56:38 -0400

    added firebase user logged in check and navigation container

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import React, {useEffect} from 'react';
 import {View, StatusBar, StyleSheet, useColorScheme} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import {NavigationContainer} from '@react-navigation/native';
 
 import colors from './app/config/colors';
-import ProfileSwiper from './app/components/Swiper';
-import Login from './app/screens/Login';
+import AppNavigator from './app/navigation/AppNavigator';
+import isUser from './app/services/user';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 const App = () => {
   useEffect(() => SplashScreen.hide(), []);
   const isDarkMode = useColorScheme() === 'dark';
+  const user = isUser();
+
+  // console.log(user === null ? 'Loading' : user.displayName);
+  const navigationRef = React.createRef();
+  // @ts-ignore
+  const {navigate} = (name, params) =>
+    // @ts-ignore
+    navigationRef.current?.navigate(name, params);
 
   return (
     <View style={styles.app}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      {/* <ProfileSwiper /> */}
-      {/* <Login /> */}
+      <NavigationContainer ref={navigate}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
     </View>
   );
 };

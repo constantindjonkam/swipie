4a639de97b9af9d792bb781ffcc0ac5485804255
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-07-29 19:32:19 -0400

    created the user service

---
+++ app/services/user.ts
@@ -0,0 +1,19 @@
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {useState, useEffect} from 'react';
+
+export default function isUser() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  return initializing ? null : user;
+}

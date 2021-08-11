c12fb32d4a6cec0cd340baff073f332930173c4e
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-08-11 01:43:07 -0400

    refactored the app component

--- App.tsx
+++ App.tsx
@@ -1,31 +1,19 @@
 import React, {useEffect} from 'react';
 import {View, StatusBar, StyleSheet, useColorScheme} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import {NavigationContainer} from '@react-navigation/native';
 
 import colors from './app/config/colors';
-import AppNavigator from './app/navigation/AppNavigator';
-import isUser from './app/services/user';
-import AuthNavigator from './app/navigation/AuthNavigator';
+import withCodePush from './withCodePush';
+import RootNavigator from './app/navigation/RootNavigator';
 
 const App = () => {
   useEffect(() => SplashScreen.hide(), []);
   const isDarkMode = useColorScheme() === 'dark';
-  const user = isUser();
-
-  // console.log(user === null ? 'Loading' : user.displayName);
-  const navigationRef = React.createRef();
-  // @ts-ignore
-  const {navigate} = (name, params) =>
-    // @ts-ignore
-    navigationRef.current?.navigate(name, params);
 
   return (
     <View style={styles.app}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer ref={navigate}>
-        {user ? <AppNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
+      <RootNavigator />
     </View>
   );
 };
@@ -37,4 +25,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default withCodePush(App);

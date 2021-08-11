1f6b978bd6ddc66b37eb1bee4c64088bb258f7d3
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-08-11 01:25:47 -0400

    added signout button

--- app/screens/AccountInfo.tsx
+++ app/screens/AccountInfo.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Button from '../components/Button';
 
 import appStyles from '../config/appStyles';
 import colors from '../config/colors';
+import {googleSignOut} from '../services/auth';
+import getUser from '../services/user';
 
 export interface AccountInfoProps {}
 
 const AccountInfo: React.FC<AccountInfoProps> = () => {
+  const user = getUser();
+
   return (
     <View style={styles.account}>
       <Text style={styles.big}>Account</Text>
+      <Text style={styles.big}>{user?.displayName}</Text>
+
+      <Button
+        title="Sign Out"
+        onPress={() =>
+          googleSignOut()
+            .then(() => console.log('Signed Out'))
+            .catch(err => console.log('Failed', err))
+        }
+      />
     </View>
   );
 };
